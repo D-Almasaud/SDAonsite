1c164ab8c93fa1837cf866ace7f2efca1c7f21c6
D-almasaud <you@example.com>
2024-03-04 16:55:43 +0300

    hw commit

--- src/test/java/HomeWorks/day09/task02.java
+++ src/test/java/HomeWorks/day09/task02.java
@@ -1,4 +1,27 @@
 package HomeWorks.day09;
 
-public class task02 {
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import utilities.TestBase;
+
+import java.time.Duration;
+
+public class task02 extends TestBase {
+    @Test
+    public void sliderTest(){
+        By slider = By.className("rangeslider__handle");
+
+        driver.get("https://rangeslider.js.org/ ");
+        WebElement sliderButton = driver.findElement(slider);
+        Actions actions= new Actions(driver);
+        actions
+                .dragAndDropBy(sliderButton,-100,0)
+                .pause(Duration.ofSeconds(2))
+                .dragAndDropBy(sliderButton,100,0)
+                .build()
+                .perform();
+
+    }
 }
--- src/test/java/HomeWorks/day09/task03.java
+++
@@ -1,4 +0,0 @@
-package HomeWorks.day09;
-
-public class task03 {
-}
