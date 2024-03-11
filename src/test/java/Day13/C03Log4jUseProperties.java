3f22d804f9d2201265e89f848509a96b9adba38a
D-almasaud <you@example.com>
2024-03-11 13:45:40 +0300

    day13 commit

---
+++ src/test/java/Day13/C03Log4jUseProperties.java
@@ -0,0 +1,24 @@
+package Day13;
+
+import org.apache.log4j.PropertyConfigurator;
+
+import org.apache.log4j.Logger;
+
+public class C03Log4jUseProperties {
+
+    public static void main(String[] args) {
+
+        PropertyConfigurator.configure("log4j.properties");
+
+        Logger logger = Logger.getLogger(C03Log4jUseProperties.class);
+
+        logger.trace("This is a trace message");
+        logger.debug("This is a debug message");
+        logger.info("This is an info message");
+        logger.warn("This is a warn message");
+        logger.error("This is an error message");
+        logger.fatal("This is a fatal message");
+
+
+    }
+}
